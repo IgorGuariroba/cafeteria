80cc7c926999bf1e5c18378bf35cf81b1c7d6954
igor.guariroba <igorguariroba@movida.com.br>
2022-12-08 22:01:36 -0300

    Coluna direita

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,14 +1,21 @@
 import { Actions, LayoutContainer } from './styles'
 import logoNav from '../../assets/Logo.svg'
-import { ShoppingCart } from "phosphor-react";
 
+import map from '../../assets/map.svg'
+import shoppingCart from '../../assets/ShoppingCart.svg'
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
       <img src={logoNav} alt="Copo de café" />
       <Actions>
-        <ShoppingCart size={32} />
+        <div>
+          <img src={map} alt="" />
+          Mogi Das Cruzes
+        </div>
+        <span>
+        <img src={shoppingCart} alt="" />
+        </span>
       </Actions>
     </LayoutContainer>
   )
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -4,9 +4,51 @@ export const LayoutContainer = styled.div`
 
   height: '104px';
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   padding: 32px 160px;
   gap: 820px;
   background: ${(props) => props.theme['white']};
 `
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0px;
+  gap: 12px;
+
+  width: 193px;
+  height: 38px;
+
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    text-align: center;
+    color: #4B2995;
+    background: #EBE5F9;
+    border-radius: 8px;
+    padding: 4px;
+    font-family: Roboto;
+    font-size: 0.9rem;
+
+    img {
+      height: 2rem
+    }
+  }
+
+  span {
+    background: #F1E9C9;
+    color: #C47F17;
+    border-radius: 8px;
+    padding: 8px;
+  }
+
+`
+
